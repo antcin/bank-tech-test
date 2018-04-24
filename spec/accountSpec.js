a19fa8dc53fb0aa71828fee6c22fe6347357194b
antcin <ciniglioantonio@gmail.com>
2018-04-25 00:21:27 +0100

    install Istanbul for code coverage

---
+++ spec/accountSpec.js
@@ -0,0 +1,65 @@
+'use strict';
+
+describe('Account', function() {
+
+  it('is an instance of Account', function() {
+    expect(account).toEqual(jasmine.any(Account))
+  });
+
+  it('has a statement property', function() {
+    expect(account.statement).toBe(statement)
+  })
+
+  describe('When account is initialised', function() {
+
+    it('balance is equal to zero', function() {
+      expect(account.balance).toEqual(0.00);
+    });
+  });
+
+  describe('#deposit', function() {
+
+    beforeEach(function() {
+      spyOn(statement, 'addTransaction');
+    });
+
+    it('increases the bank balance', function() {
+      account.deposit(100.00);
+      expect(account.balance).toEqual(100.00);
+    });
+
+    it('adds deposit transaction to statement', function() {
+      account.deposit(100.00);
+      expect(account.statement.addTransaction).toHaveBeenCalled();
+    })
+  });
+
+  describe('#withdraw', function() {
+
+    beforeEach(function() {
+      spyOn(statement, 'addTransaction')
+    });
+
+    it('decreases the bank balance', function() {
+      account.withdraw(50.00)
+      expect(account.balance).toEqual(-50.00)
+    });
+
+    it('adds withdraw transaction to statement', function() {
+      account.withdraw(50.00)
+      expect(account.statement.addTransaction).toHaveBeenCalled();
+    });
+  });
+
+  describe('#displayStatement', function() {
+
+    beforeEach(function(){
+      spyOn(console, 'log');
+    });
+
+    it('returns a string', function() {
+      account.displayStatement();
+      expect(console.log).toHaveBeenCalled();
+    });
+  });
+});
